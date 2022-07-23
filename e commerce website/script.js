10a93cd96a11b6ea830723a35628dbdb3456f542
MrAtulPal <100793099+MrAtulPal@users.noreply.github.com>
2022-07-23 18:38:28 +0530

    Add files via upload

---
+++ e commerce website/script.js
@@ -0,0 +1,18 @@
+var mainimg = document.getElementById("main-img");
+var smallimg = document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function(){
+    mainimg.src = smallimg.src;
+}
+
+smallimg[1].onclick = function(){
+    mainimg.src = smallimg.src;
+}
+
+smallimg[2].onclick = function(){
+    mainimg.src = smallimg.src;
+}
+
+smallimg[3].onclick = function(){
+    mainimg.src = smallimg.src;
+}
